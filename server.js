3ee5e0939d9b02a2d19ade7d4decfd3b7929c72a
elson lim <elson.lim@autodesk.com>
2016-09-06 19:48:33 +0800

    generateViewTest endpoint support only click and expect text

--- server.js
+++ server.js
@@ -3,19 +3,30 @@ var app = express();
 var portNumber = 3000;
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-var viewTest = require('./viewTest.js')();
-
-console.log(viewTest);
+var viewTest = require('./viewTest.js');
 
 app.use(express.static('public'));
-
 
 app.get('/', function (req, res) {
 	res.sendfile('./public/index.html');
 });
 
+app.post('/generateViewTest', jsonParser, function (req, res) {	
+	console.log('generateViewTest');
+	
+	var fileName = 'viewTest';
+	
+	res.writeHead(200, {
+		'Content-Type': 'application/force-download',
+		'Content-disposition': 'attachment; filename=' + fileName + '.js'
+	});
+
+	res.end(viewTest(req.body));
+});
+
 app.post('/echoJson', jsonParser, function (request, response) {
 	console.log(request.body); // your JSON
+
 	response.send(request.body); // echo the result back
 });
 
