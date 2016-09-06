f330f5dc0a0239731b9d52aa8ff84c5baf26f11f
elson lim <elson.lim@autodesk.com>
2016-09-06 11:08:08 +0800

    create basic node server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var portNumber = 3000;
+
+app.get('/', function (req, res) {
+	res.sendfile('./public/index.html');
+});
+
+app.use(express.static('public'));
+
+app.listen(portNumber, function () {
+	console.log('Example app liatening on port ' + portNumber + '!');
+});
